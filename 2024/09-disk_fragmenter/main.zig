216c3335abc7c34d971455eb9c1a6ccd74666f6c
mzwallow <punnarat_mos@hotmail.com>
2024-12-12 20:17:22 +0700

    feat(2024): day 9

--- 2024/09-disk_fragmenter/main.zig
+++ 2024/09-disk_fragmenter/main.zig
@@ -11,6 +11,7 @@ pub fn main() !void {
     const allocator = gpa.allocator();
 
     const file = try std.fs.cwd().openFile("input.txt", .{});
+    // const file = try std.fs.cwd().openFile("example.txt", .{});
     defer file.close();
 
     var buffered = std.io.bufferedReader(file.reader());
@@ -22,8 +23,14 @@ pub fn main() !void {
         disk_map.deinit();
     }
 
-    var file_id_map = HashMap(u128, usize).init(allocator);
-    defer file_id_map.deinit();
+    var input = ArrayList(u8).init(allocator);
+    defer input.deinit();
+
+    var files = ArrayList(u8).init(allocator);
+    defer files.deinit();
+
+    var spaces = ArrayList(u8).init(allocator);
+    defer spaces.deinit();
 
     var buf: [20480]u8 = undefined;
     while (try buf_reader.readUntilDelimiterOrEof(&buf, '\n')) |line| {
@@ -31,7 +38,11 @@ pub fn main() !void {
         for (line, 0..) |char, i| {
             const block = try std.fmt.parseInt(u8, &[_]u8{char}, 10);
 
+            try input.append(block);
+
             if (try isEven(i)) {
+                try files.append(block);
+
                 var repeat: u8 = 0;
                 while (repeat < block) : (repeat += 1) {
                     const block_id_str = try std.fmt.allocPrint(allocator, "{d}", .{block_id});
@@ -40,6 +51,8 @@ pub fn main() !void {
 
                 block_id += 1;
             } else {
+                try spaces.append(block);
+
                 var repeat: u8 = 0;
                 while (repeat < block) : (repeat += 1) {
                     const dot = try std.fmt.allocPrint(allocator, ".", .{});
@@ -78,7 +91,7 @@ pub fn main() !void {
     }
 
     const checksum_1 = try partOne(&disk_map_1);
-    const checksum_2 = try partTwo(&disk_map_2);
+    const checksum_2 = try partTwo(input.items, disk_map_2, files.items, spaces.items);
 
     print("Part 1: {d}\n", .{checksum_1});
     // 88934811716: FAILED - Too low
@@ -87,14 +100,17 @@ pub fn main() !void {
 
     print("Part 2: {d}\n", .{checksum_2});
     // 12838265093415: FAILED - Too high
+    // 12745090777388: FAILED - Too high
+    // 12719358014229: FAILED - Too high
+    // 6250605700557: PASSED
 }
 
 fn partOne(disk_map: *[][]u8) !u128 {
-    print("=====================================\n", .{});
-    for (disk_map.*) |char| {
-        print("{s}", .{char});
-    }
-    print("\n=====================================\n", .{});
+    // print("=====================================\n", .{});
+    // for (disk_map.*) |char| {
+    //     print("{s}", .{char});
+    // }
+    // print("\n=====================================\n", .{});
 
     {
         var i: usize = 0;
@@ -108,116 +124,11 @@ fn partOne(disk_map: *[][]u8) !u128 {
                 const tmp = disk_map.*[i];
                 disk_map.*[i] = disk_map.*[j];
                 disk_map.*[j] = tmp;
-                // print("{s} i:{d} j:{d}\n", .{ disk_map.*, i, j });
             }
 
             if (i + 1 == j) break;
 
             i += 1;
-        }
-
-        print("=====================================\n", .{});
-        for (disk_map.*) |char| {
-            print("{s}", .{char});
-        }
-        print("\n=====================================\n", .{});
-    }
-
-    var last_block_idx: usize = undefined;
-    {
-        var i = disk_map.len - 1;
-        while (true) : (i -= 1) {
-            if (!std.mem.eql(u8, disk_map.*[i], ".")) {
-                last_block_idx = i;
-                break;
-            }
-        }
-    }
-
-    var checksum: u128 = 0;
-    for (disk_map.*, 0..) |char, i| {
-        const block: u128 = try std.fmt.parseInt(u128, char, 10);
-
-        checksum += i * @as(@TypeOf(checksum), @intCast(block));
-        // print("{d}: ({d} * {d})\n", .{ checksum, i, block });
-
-        if (i == last_block_idx) break;
-    }
-
-    return checksum;
-}
-
-fn partTwo(disk_map: *[][]u8) !u128 {
-    // print("=====================================\n", .{});
-    // for (disk_map.*) |char| {
-    //     print("{s}", .{char});
-    // }
-    // print("\n=====================================\n", .{});
-
-    {
-        var latest_file_id: usize = undefined;
-        var latest_file_id_idx: usize = undefined;
-        {
-            var idx = disk_map.len - 1;
-            while (true) : (idx -= 1) {
-                if (!std.mem.eql(u8, disk_map.*[idx], ".")) {
-                    latest_file_id_idx = idx;
-                    latest_file_id = try std.fmt.parseInt(@TypeOf(latest_file_id), disk_map.*[idx], 10);
-                    break;
-                }
-            }
-        }
-
-        var i: usize = 0;
-        outer: while (latest_file_id > 0) {
-            if (!std.mem.eql(u8, disk_map.*[i], ".")) {
-                i += 1;
-            } else {
-                var cur_space_idx = i;
-                var spaces: usize = 0;
-                while (std.mem.eql(u8, disk_map.*[cur_space_idx], ".")) : (cur_space_idx += 1) {
-                    spaces += 1;
-                }
-
-                print("i:{d} spaces:{d}\n", .{ i, spaces });
-
-                while (latest_file_id > 0) : (latest_file_id -= 1) {
-                    latest_file_id_idx = try lastIndexOf(disk_map, latest_file_id);
-
-                    var cur_latest_file_id_idx = latest_file_id_idx;
-                    const cur_latest_file_id_str = disk_map.*[cur_latest_file_id_idx];
-                    var file_id_count: usize = 0;
-                    while (std.mem.eql(u8, disk_map.*[cur_latest_file_id_idx], cur_latest_file_id_str)) : (cur_latest_file_id_idx -= 1) {
-                        file_id_count += 1;
-                    }
-
-                    print("latest_file_id:{d} idx:{d} count:{d}\n", .{ latest_file_id, latest_file_id_idx, file_id_count });
-
-                    if (spaces >= file_id_count) {
-                        var iter_j: usize = 0;
-                        while (iter_j < file_id_count) : (iter_j += 1) {
-                            const tmp = disk_map.*[i + iter_j];
-                            disk_map.*[i + iter_j] = disk_map.*[latest_file_id_idx - iter_j];
-                            disk_map.*[latest_file_id_idx - iter_j] = tmp;
-                        }
-
-                        // for (disk_map.*) |char| {
-                        //     print("{s}", .{char});
-                        // }
-                        // print("\n", .{});
-
-                        i += spaces;
-                        latest_file_id -= 1;
-                        latest_file_id_idx = try lastIndexOf(disk_map, latest_file_id);
-
-                        if (std.mem.eql(u8, disk_map.*[i], disk_map.*[latest_file_id_idx])) {
-                            i = 0;
-                        }
-
-                        continue :outer;
-                    }
-                }
-            }
         }
 
         // print("=====================================\n", .{});
@@ -240,13 +151,59 @@ fn partTwo(disk_map: *[][]u8) !u128 {
 
     var checksum: u128 = 0;
     for (disk_map.*, 0..) |char, i| {
-        if (std.mem.eql(u8, char, ".")) continue;
-
         const block: u128 = try std.fmt.parseInt(u128, char, 10);
 
         checksum += i * @as(@TypeOf(checksum), @intCast(block));
 
         if (i == last_block_idx) break;
+    }
+
+    return checksum;
+}
+
+fn partTwo(disk_map: []u8, blocks: [][]u8, files: []u8, spaces: []u8) !u128 {
+    // print("blocks: {s}\n", .{blocks});
+    // print("disk_map: {any}\n", .{disk_map});
+    // print("files: {any}\n", .{files});
+    // print("spaces: {any}\n", .{spaces});
+
+    var file_id: usize = files.len - 1;
+    while (file_id > 0) : (file_id -= 1) {
+        for (spaces, 0..) |space, space_idx| {
+            // print("files:{d} file_id:{d}\t", .{ files[file_id], file_id });
+            // print("space:{d} space_idx:{d}\n", .{ space, space_idx });
+            if (space >= files[file_id]) {
+                const first_file_idx = sum(disk_map[0 .. file_id * 2]);
+                const first_space_idx = sum(disk_map[0 .. space_idx * 2 + 1]);
+                if (first_space_idx >= first_file_idx) break;
+
+                // print("\tfirst_file_idx: {d}\n", .{first_file_idx});
+                // print("\tfirst_space_idx: {d}\n", .{first_space_idx});
+
+                // Swap
+                for (0..files[file_id]) |i| {
+                    const tmp = blocks[first_space_idx + i];
+                    blocks[first_space_idx + i] = blocks[first_file_idx + i];
+                    blocks[first_file_idx + i] = tmp;
+                }
+
+                disk_map[space_idx * 2 + 1] -= files[file_id];
+                disk_map[space_idx * 2] += files[file_id];
+                disk_map[file_id * 2] = 0;
+
+                spaces[space_idx] -= files[file_id];
+                break;
+            }
+        }
+    }
+
+    var checksum: u128 = 0;
+    for (blocks, 0..) |char, i| {
+        if (std.mem.eql(u8, char, ".")) continue;
+
+        const block: u128 = try std.fmt.parseInt(u128, char, 10);
+
+        checksum += i * @as(@TypeOf(checksum), @intCast(block));
     }
 
     return checksum;
@@ -259,12 +216,8 @@ fn isEven(value: usize) !bool {
     return false;
 }
 
-fn lastIndexOf(disk: *[][]u8, file_id: usize) !usize {
-    var idx: usize = disk.len - 1;
-    var buf: [8]u8 = undefined;
-    while (true) : (idx -= 1) {
-        if (std.mem.eql(u8, disk.*[idx], try std.fmt.bufPrint(&buf, "{d}", .{file_id}))) {
-            return idx;
-        }
-    }
+fn sum(xs: []u8) usize {
+    var result: usize = 0;
+    for (xs) |x| result += @intCast(x);
+    return result;
 }
